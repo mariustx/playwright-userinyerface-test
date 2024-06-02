9738a49642233deae2c28d30198a5f8559a37ec3
venom <spidyvenom@mail.com>
2024-06-02 21:04:47 +0300

    update selectors

--- page-objects/page3-class.ts
+++ page-objects/page3-class.ts
@@ -12,6 +12,7 @@ export class Page3 extends Forms {
     private readonly titleFemaleOption: string;
     private readonly numberField: string;
     private readonly numberFieldUp: string;
+    private readonly numberFieldDown: string;
     private readonly ageSlider: string;
     private readonly countryDropdown: string;
     private readonly countryAXOption: string;
@@ -28,25 +29,26 @@ export class Page3 extends Forms {
 
     constructor(page: Page) {
         super(page);
-        this.firstNameField = "//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]";
-        this.zipField = "//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/input[1]";
-        this.cityField = "//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/input[1]";
-        this.surnameField = "//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]";
-        this.streetField = "//body/div[@id='app']/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/input[1]";
-        this.titleDropdown = "//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]";
-        this.titleMaleOption = "//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]";
-        this.titleFemaleOption = "//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]";
-        this.numberField = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]';
-        this.numberFieldUp = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]/span[1]';
+        this.firstNameField = ".personal-details__tr-row:first-of-type .personal-details__td-cell:nth-of-type(1) input";
+        this.zipField = ".personal-details__tr-row:first-of-type .personal-details__td-cell:nth-of-type(2) input";
+        this.cityField = ".personal-details__tr-row:nth-of-type(2) .personal-details__td-cell:nth-of-type(2) input";
+        this.surnameField = ".personal-details__tr-row:nth-of-type(3) .personal-details__td-cell:nth-of-type(1) input";
+        this.streetField = ".personal-details__tr-row:nth-of-type(4) .personal-details__td-cell:nth-of-type(1) input";
+        this.titleDropdown = ".personal-details__tr-row:nth-of-type(2) .personal-details__td-cell:nth-of-type(1) ";
+        this.titleMaleOption = ".personal-details__tr-row:nth-of-type(2) .personal-details__td-cell:nth-of-type(1) .dropdown__list-item:nth-of-type(1)";
+        this.titleFemaleOption = ".personal-details__tr-row:nth-of-type(2) .personal-details__td-cell:nth-of-type(1) .dropdown__list-item:nth-of-type(2)";
+        this.numberField = '.personal-details__tr-row:nth-of-type(5) .personal-details__td-cell:nth-of-type(1) ';
+        this.numberFieldUp = '.personal-details__tr-row:nth-of-type(5) .personal-details__td-cell:nth-of-type(1) .icon-chevron-up';
+        this.numberFieldDown = '.personal-details__tr-row:nth-of-type(5) .personal-details__td-cell:nth-of-type(1) .icon-chevron-down';
         this.ageSlider = '.slider__handle';
         this.countryDropdown = '.country-dropdown__selected-country-name';
         this.countryAXOption = '.flag-ax';
-        this.boxField = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]';
-        this.boxFieldUp = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/button[1]/span[1]';
-        this.boxFieldDown = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/button[2]/span[1]';
-        this.birthDayDropdown = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[5]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]';
-        this.birthMonthDropdown = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[5]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]';
-        this.birthYearDropdown = '//body[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[5]/div[2]/div[1]/div[1]/div[2]/div[1]/div[3]/div[1]/div[1]/div[1]';
+        this.boxField = '.personal-details__tr-row:nth-of-type(4) .personal-details__td-cell:nth-of-type(2)';
+        this.boxFieldUp = '.personal-details__tr-row:nth-of-type(4) .personal-details__td-cell:nth-of-type(2) .icon-chevron-up';
+        this.boxFieldDown = '.personal-details__tr-row:nth-of-type(4) .personal-details__td-cell:nth-of-type(2) .icon-chevron-down';
+        this.birthDayDropdown = '.date-dropdown__container--day .icon-chevron-down';
+        this.birthMonthDropdown = '.date-dropdown__container--month .icon-chevron-down';
+        this.birthYearDropdown = '.date-dropdown__container--year .icon-chevron-down';
         this.maleToggleButton = '.toggle-button--left';
         this.femaleToggleButton = '.toggle-button--right';
         this.nextButton = ':nth-child(1) > .button--white';
@@ -72,6 +74,8 @@ export class Page3 extends Forms {
         await page.getByText(birthMonth, { exact: true }).click();
         await page.locator(this.birthYearDropdown).click();
         await page.getByText(birthYear, { exact: true }).click();
+
+
         await page.locator(this.nextButton).click();
 
         // need to find a way to move slider
@@ -81,4 +85,24 @@ export class Page3 extends Forms {
     countryPicker(countryCode: string) {
         return `.flag-${countryCode}`;
     }
+
+    async moveSlider(page: any): Promise<void> {
+        const sliderTrack = await page.locator('.slider__handle').first();
+        const handlePosition = await sliderTrack.evaluate(el => {
+            return {
+                x: el.getBoundingClientRect().x,
+                y: el.getBoundingClientRect().y
+            };
+        });
+    
+        console.log(handlePosition);
+    
+        // Using the hover method to place the mouse cursor, then moving it to the right
+        await sliderTrack.hover({ force: true, position: handlePosition });
+        await page.mouse.down();
+        await sliderTrack.hover({ force: true, position: { x: handlePosition.x + 30, y: handlePosition.y } });
+        await page.mouse.up();
+    }
+    
+
 }
